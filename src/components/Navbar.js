89ee4a4d2c1528457d29b648a264033e5118ef7b
Tom C Watson <tom.watson@dataforaction.org.uk>
2025-06-09 16:57:55 +0100

    Revert "Added option steps and mobile styling"
    This reverts commit 22080865c71d9d216b805427df10daeab8c8030d.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,13 +6,13 @@ function Navbar({ onHomeClick, onAddQuestionClick, onSearchClick, onAddAnswerCli
         <div className="navbar">
             <div>
             <img src={logoImage} width="60" height="60"></img>
-            
+        
             
             </div>
-            <h2 class="title">Question Bank</h2>
+            <h2 class="purple">Question Bank</h2>
             
             
-            <ul class="navli">
+            <ul class="purple">
         <li onClick={onHomeClick}>Home</li>
         <li onClick={onAddQuestionClick}>Add Question</li>
         <li onClick={onSearchClick}>Search Questions</li>
